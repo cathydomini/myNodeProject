4d0589207d33129b2e9800fb9339bd6e544ae1a6
Cathy Dominioni <cathydominioni@cathys-mbp.attlocal.net>
2016-11-12 19:50:12 -0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET home page. using jade*/
+/*
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+*/
+
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
+});
+ 
+
+module.exports = router;
